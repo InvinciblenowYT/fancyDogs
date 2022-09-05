eec3ca8017df2a896422a86a8bb58f207f41c2df
uwu <uwu@MacBook-Pro-von-uwu.local>
2022-09-05 16:53:52 +0200

    initial commit

---
+++ src/main/java/ch/bbw/leoluca/LeoLucaApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.leoluca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoLucaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeoLucaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/leoluca/LeoLucaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.leoluca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeoLucaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
