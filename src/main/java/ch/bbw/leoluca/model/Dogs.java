8e7a9a767ab68a3c6b5dac781bb00810db8fe993
Leo Roettele <leo.roettele@liip.ch>
2022-09-25 20:12:40 +0200

    use FetchType.EAGER because otherwise the get requests won't work

--- src/main/java/ch/bbw/leoluca/model/Dogs.java
+++ src/main/java/ch/bbw/leoluca/model/Dogs.java
@@ -11,7 +11,7 @@ public class Dogs {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "halter_id")
     private Owners owners;
     private String rasse1;
