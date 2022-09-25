ff0317cd30e944eb7fdb80ff80adfbd845102299
Leo Roettele <leo.roettele@liip.ch>
2022-09-25 21:02:20 +0200

    create Rest Endpoints

---
+++ src/main/java/ch/bbw/leoluca/controller/DogsController.java
@@ -0,0 +1,39 @@
+package ch.bbw.leoluca.controller;
+
+import ch.bbw.leoluca.model.Dogs;
+import ch.bbw.leoluca.repository.DogsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class DogsController {
+    @Autowired
+    private final DogsRepository dogsRepository;
+
+    public DogsController(DogsRepository dogsRepository) {
+        this.dogsRepository = dogsRepository;
+    }
+
+    @GetMapping("/dogs")
+    List<Dogs> all() {
+        return dogsRepository.findAll();
+    }
+
+    @GetMapping("/dogs/year={year}")
+    List<Dogs> DogsByYear(@PathVariable("year") String year){
+        return dogsRepository.dogsByYear(Integer.parseInt(year));
+    }
+
+    @GetMapping("/dogs/dogsex={dogsex}")
+    List<Dogs> DogsBySex(@PathVariable("dogsex") String sex){
+        return dogsRepository.dogsBySex(sex);
+    }
+
+    @GetMapping("/dogs/ownergender={ownergender}")
+    List<Dogs> DogsByOwnerGender(@PathVariable("ownergender") String gender){
+        return dogsRepository.dogsByOwnerGender(gender);
+    }
+}
