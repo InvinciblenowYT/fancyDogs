0a89eb0adb05c5138b50079990982d68ef777d74
Leo Roettele <leo.roettele@liip.ch>
2022-09-25 21:02:09 +0200

    create DogsRepository with a few queries

--- src/main/java/ch/bbw/leoluca/repository/DogsRepository.java
+++ src/main/java/ch/bbw/leoluca/repository/DogsRepository.java
@@ -4,14 +4,9 @@ import ch.bbw.leoluca.model.Dogs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
-
 public interface DogsRepository extends JpaRepository<Dogs, Integer> {
-/*
-    List<Dogs> findBygeburtsjahr_hundEquals(Integer year);
-*/
     @Query("SELECT d FROM dogs d WHERE d.geburtsjahr_hund = :year")
     List<Dogs>dogsByYear(@Param("year") Integer year);
 
