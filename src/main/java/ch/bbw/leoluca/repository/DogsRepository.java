a40b88f63bf19d216083b7009811fac10a4961be
uwu <uwu@MacBook-Pro-von-uwu.local>
2022-09-26 11:08:35 +0200

    added new query

--- src/main/java/ch/bbw/leoluca/repository/DogsRepository.java
+++ src/main/java/ch/bbw/leoluca/repository/DogsRepository.java
@@ -1,12 +1,30 @@
 package ch.bbw.leoluca.repository;
 
 import ch.bbw.leoluca.model.Dogs;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface DogsRepository extends JpaRepository<Dogs, Integer> {
+
+    // Select with 2 wheres
+
+    @Query();
+
+    // Select with Limit (spring paging)
+
+    @Query(value = "SELECT d FROM dogs d limit ?1, ?2", nativeQuery = true)
+    List<Dogs> findDogs(int offset, int limit);
+
+    // Page<Dogs> allDogs =
+    // Delete with where
+
+    // Join
+
+    // complicated select
+
     @Query("SELECT d FROM dogs d WHERE d.geburtsjahr_hund = :year")
     List<Dogs>dogsByYear(@Param("year") Integer year);
 
